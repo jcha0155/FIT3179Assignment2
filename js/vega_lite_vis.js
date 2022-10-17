7bdade3514f624d2f73669a4331acf66f5dabe9f
Chan Jian Sen <114260201+jcha0155@users.noreply.github.com>
2022-10-17 16:34:42 +0800

    add c
    add c

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -2,7 +2,7 @@ var spec1 = "js/assgm2chart1.json";
 var spec2 = "js/assgm2chart2.json";
 var spec3 = "js/assgm2chart3.json";
 var spec4 = "js/assgm2chart4&5.json";
-var spec6 = "js/assgm2chart6.json";
+var spec6 = "js/assgm2chart6c.json";
 
 vegaEmbed('#chart1', spec1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
